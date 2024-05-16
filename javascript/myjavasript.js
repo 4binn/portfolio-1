3d9ae3c875e7d0090908325d45ffd242d0eca636
4binn <bintangstu56@gmail.com>
2024-05-16 11:00:30 +0700

    first commit

---
+++ javascript/myjavasript.js
@@ -0,0 +1,17 @@
+// toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Ketika Menu di klik
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik diluar navbar
+
+const menu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
